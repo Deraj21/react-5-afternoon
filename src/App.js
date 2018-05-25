99f46da5b0f3b31f45c6704ceb7f7007ea63562f
deraj21 <deraj21@gmail.com>
2018-05-25 14:47:48 -0700

    done with step 3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 import router from './router'
 
+/*
+
+Open src/App.js.
+Remove the invoking parenthesis from your {router} as well as the content inside of them.
+*/
 
 class App extends Component {
   constructor(){
@@ -143,11 +148,7 @@ class App extends Component {
   render() {
     return (
       <div>
-    
-        { 
-          router( this.state.loanType, this.state.propertyType, this.handleChangeLoanType, this.handleChangePropertyType, this.handleChangePropertyToBeUsedOn, this.state.propToBeUsedOn, this.state.city, this.handleChangeCity, this.handleChangeFoundFalse, this.handleChangeFoundTrue, this.state.found, this.handleChangeRealEstateAgentTrue, this.handleChangeRealEstateAgentFalse, this.state.realEstateAgent, this.handleChangeUpdateDownPayment, this.state.downPayment, this.handleChangeUpdateCost, this.state.cost, this.state.credit, this.handleChangeCreditE, this.handleChangeCreditG,this.handleChangeCreditF, this.handleChangeCreditP, this.state.history, this.handleChangeUpdateHistory, this.state.addressOne, this.state.addressTwo, this.state.addressThree, this.handleChangeAddressOne, this.handleChangeAddressTwo, this.handleChangeAddressThree, this.handleChangeFirstName, this.handleChangeLastName, this.handleChangeEmail, this.state.firstName, this.state.lastName, this.state.email ) 
-        }
-
+        { router }
       </div>
     );
   }
